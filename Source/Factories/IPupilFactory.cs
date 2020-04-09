d75dac1b848c523c794172436c22c9ef99b6cecb
Chris Tamlyn <chris.tamlyn@bridgeinternationalacademies.com>
2020-04-09 12:09:00 +0100

    Add pupil factory

---
+++ Source/Factories/IPupilFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Grade.Promoter.Factories
+{
+    using System.Collections.Generic;
+    using Grade.Promoter.Models;
+    using Grade.Promoter.ViewModels;
+
+    public interface IPupilFactory
+    {
+        Pupil Create(List<ExamResult> list);
+    }
+}
