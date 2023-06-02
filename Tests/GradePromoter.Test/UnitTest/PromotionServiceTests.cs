62675f38723fe0817260842ec0bc8370508788a6
Dragos Raducanu <dragos.raducanu@newglobe.education>
2023-06-02 15:23:25 +0200

    Removes test implementation and adds Results.txt to .gitignore

--- Tests/GradePromoter.Test/UnitTest/PromotionServiceTests.cs
+++ Tests/GradePromoter.Test/UnitTest/PromotionServiceTests.cs
@@ -1,10 +1,6 @@
 namespace Grade.Promoter.Test.UnitTest
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using Grade.Promoter.Services;
-    using Grade.Promoter.ViewModels;
     using Shouldly;
     using Xunit;
 
@@ -18,69 +14,14 @@ namespace Grade.Promoter.Test.UnitTest
         }
 
         [Fact]
-        public void PupilWithGreaterThan50PercentAverage_ReturnsPromoted()
-        {
-            var examResults = new List<ExamResult>
-            {
-                new ExamResult
-                {
-                    PupilId = 1,
-                    Grade = "Standard 1",
-                    AssesmentDate = default(DateTime),
-                    AssessmentType = "End-Term",
-                    PupilName = "Joe Bloggs",
-                    Result = 70,
-                    Subject = "Verbal Reasoning",
-                },
-                new ExamResult
-                {
-                    PupilId = 1,
-                    Grade = "Standard 1",
-                    AssesmentDate = default(DateTime),
-                    AssessmentType = "End-Term",
-                    PupilName = "Joe Bloggs",
-                    Result = 45,
-                    Subject = "Maths",
-                },
-            };
-
-            var results = this.promotionService.GetPromotionResults(examResults);
-            results.Count.ShouldBe(1);
-            var result = results.Single();
-            result.Promoted.ShouldBeTrue();
+        public void OneIsOne() {
+            1.ShouldBe(1);
         }
 
         [Fact]
-        public void PupilWithLessThan50PercentAverage_ReturnsNotPromoted()
+        public void PupilWithGreaterThan50PercentAverage_ReturnsPromoted()
         {
-            var examResults = new List<ExamResult>
-            {
-                new ExamResult
-                {
-                    PupilId = 1,
-                    Grade = "Standard 1",
-                    AssesmentDate = default(DateTime),
-                    AssessmentType = "End-Term",
-                    PupilName = "Joe Bloggs",
-                    Result = 51,
-                    Subject = "Maths",
-                },
-                new ExamResult
-                {
-                    PupilId = 1,
-                    Grade = "Standard 1",
-                    AssesmentDate = default(DateTime),
-                    AssessmentType = "End-Term",
-                    PupilName = "Joe Bloggs",
-                    Result = 49,
-                    Subject = "Verbal Reasoning",
-                },
-            };
-
-            var results = this.promotionService.GetPromotionResults(examResults);
-            results.Count.ShouldBe(1);
-            var result = results.Single();
-            result.Promoted.ShouldBeFalse();
         }
     }
+
 }
